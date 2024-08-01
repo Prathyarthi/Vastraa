6f9518787f983099e26706ff9e3b0bbe45a628ad
Prathyarthi <prathyarti@gmail.com>
2024-08-01 22:04:05 +0530

    Removed banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import Banner from "./components/Banner"
 import Client_section from "./components/Client_section"
 import Costume from "./components/Costume"
 import Event from "./components/Event"
@@ -17,9 +16,6 @@ function App() {
       <div className="min-h-screen ml-[16px] mr-[16px] lg:ml-[150px] lg:mr-[150px] font-madeTommy bg-[#FBFFFE]">
         <Hero />
       </div>
-      {/* <div className="w-full flex md:items-end md:-mt-56">
-        <Banner />
-      </div> */}
       <div className="ml-[16px] mr-[16px] lg:ml-[150px] lg:mr-[150px] font-madeTommy bg-[#FBFFFE]">
         <Client_section />
         <Costume />
