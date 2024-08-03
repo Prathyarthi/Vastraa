1098b47ebea5680c179c4dd656e51aa3897f878d
Prathyarthi <prathyarti@gmail.com>
2024-08-04 01:45:02 +0530

    Added Features

---
+++ src/components/Features.tsx
@@ -0,0 +1,51 @@
+import { BadgePercent, Shapes, Sparkles, Star } from "lucide-react"
+import FeatureCard from "./FeatureCard"
+
+
+const features = [
+    {
+        icon: <BadgePercent />,
+        title: "Cost Effective",
+        description: "Vastraa offers premium costumes for rent at affordable prices, providing high value without compromising on quality."
+    },
+    {
+        icon: <Sparkles />,
+        title: "Easy Rentals",
+        description: "Experience a seamless and straightforward rental process with Vastraa, making it easy to get the costume you need."
+    },
+    {
+        icon: <Star />,
+        title: "Selection",
+        description: "Vastraa provides a curated collection of top-quality costumes, ensuring the ideal choice for every special event and occasion."
+    },
+    {
+        icon: <Shapes />,
+        title: "Collections",
+        description: "Vastraa's extensive costume selection caters to diverse themes and events, offering something perfect for every unique requirement."
+    },
+]
+
+function Features() {
+    return (
+        <section className="mb-32">
+            <div className="">
+                <div className="flex space-x-4">
+                    <div className="bg-gradient lg:h-[110px] lg:min-w-[8px] min-w-[8px] border rounded-full h-48"></div>
+                    <div className="flex flex-col">
+                        <div className="space-y-4">
+                            <h1 className="text-[#1B1B1E] font-bold text-4xl text-primary">Why Vastraa?</h1>
+                            <p className="text-[#1B1B1E] font-medium">Vastraa is more than just a costume provider; we are your partners in creating unforgettable experiences. Our extensive collection and personalized service ensure you find the perfect attire for any event.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="lg:grid lg:grid-cols-4 flex-col items-center space-y-5 lg:space-y-0 justify-center mt-12 lg:gap-[50px]">
+                    {features.map((feature) => (
+                        <FeatureCard key={feature.title} icon={feature.icon} title={feature.title} description={feature.description} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Features
