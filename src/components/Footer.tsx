749e75e552534671b75b1f94467b7aef36f2ca5a
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-12-10 13:51:54 +0530

    Change mobile number and fix few css properties

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,71 +1,148 @@
-import { MapPin } from 'lucide-react'
-import vastraaLogo from "../assets/footer_logo.png"
-import { Link } from 'react-router-dom'
+import { MapPin } from "lucide-react";
+import vastraaLogo from "../assets/footer_logo.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
-    return (
-        <section id='contact' className='md:pt-40 pt-24'>
-            <div className="bg-primary md:px-[150px] pt-5 px-[16px] pb-10 text-[#FBFFFE] lg:h-[400px] lg:place-content-center">
-                <div className='lg:flex space-y-8 lg:space-y-0 justify-between'>
-                    <div className='hidden md:block col-span-4'>
-                        <img src={vastraaLogo} alt="vastraa_logo" className='lg:w-[130px] w-[100px]' />
+  return (
+    <section id="contact" className="md:pt-40 pt-24">
+      <div className="bg-primary md:px-[150px] pt-5 px-[16px] pb-10 text-[#FBFFFE] lg:h-[400px] lg:place-content-center">
+        <div className="lg:flex space-y-8 lg:space-y-0 justify-between">
+          <div className="hidden md:block col-span-4">
+            <img
+              src={vastraaLogo}
+              alt="vastraa_logo"
+              className="lg:w-[130px] w-[100px]"
+            />
 
-                        <div className='pt-5'>
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d308.6555004909462!2d77.58361858441181!3d12.933331278119814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae15b504aca34b%3A0x19c69ea8046898c!2sVastraa%20Costumes%20Jayanagar!5e1!3m2!1sen!2sin!4v1727372412427!5m2!1sen!2sin" width="150" height="150" style={{ border: "0" }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Vastraa"></iframe>
-                        </div>
-                    </div>
-                    <div className='lg:flex lg:space-x-20 space-y-8 lg:space-y-0'>
-                        <div className='lg:flex flex-col items-center space-y-2 hidden'>
-                            <a href="https://www.facebook.com/vastraacostumes" target='_blank'><img className='w-[50px]' src="/facebook.svg" alt="facebook" /></a>
-                            <a href="https://www.instagram.com/vastraacostumes/" target='_blank'><img className='w-[50px]' src="/instagram.svg" alt="instagram" /></a>
-                            <a href="" target='_blank'><img className='w-[50px]' src="/linkedIn.svg" alt="linkedIn" /></a>
-                        </div>
-                        <div className='md:flex space-x-20 hidden col-span-2'>
-                            <div className='lg:flex lg:flex-col lg:space-y-7'>
-                                <a href="#costumes"><p className='hover:text-gradient'>Costumes</p></a>
-                                <a href="#events"><p className='hover:text-gradient'>Event Management</p></a>
-                                <a href="#whyVastraa"><p className='hover:text-gradient'>Why Vastraa?</p></a>
-                                <a href="#team"><p className='hover:text-gradient'>Team</p></a>
-                                <Link to="/blogs" onClick={() => window.scrollTo(0, 0)}><p className='hover:text-gradient'>Blogs</p>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className='flex flex-col space-y-5 col-span-3'>
-                            <div className='flex space-x-2 items-center'>
-                                {<MapPin className='text-[#00AC47]' />}
-                                <h2 className='text-[#FFFFFF] md:font-semibold font-bold lg:text-2xl'>Jayanagar</h2>
-                            </div>
-                            <p className='text-[14px]'>#69, 9th Main Rd,<br /> Jayanagar 3rd Block East,<br /> Bengaluru, Karnataka <br />
-                                560011</p>
-                            <div>
-                                <p className='font-bold flex flex-col text-[14px]'>+91-9986178988</p>
-                                <a href='' className='cursor-pointer'>vastraaevents@gmail.com
-                                </a>
-                            </div>
-                        </div>
-                        <div className='flex flex-col space-y-5 col-span-3'>
-                            <div className='flex space-x-2 items-center'>
-                                {<MapPin className='text-[#00AC47]' />}
-                                <h2 className='text-[#FFFFFF] md:font-semibold font-bold lg:text-2xl'>Whitefiled</h2>
-                            </div>
-                            <p className='text-[14px]'>No A/2, Inner Cir Rd,<br /> Whitefield, Dodsworth Layout,<br /> Bengaluru,
-                                Karnataka <br /> 560066</p>
-                            <div>
-                                <p className='flex flex-col font-bold text-[14px]'>+91-9986178988</p>
-                                <a href='' className='cursor-pointer'>vastraaevents@gmail.com</a>
-                            </div>
-
-                            <div className='flex space-x-3 md:hidden'>
-                                <a href="https://www.facebook.com/vastraacostumes" target='_blank'><img className='w-[50px]' src="/facebook.svg" alt="facebook" /></a>
-                                <a href="https://www.instagram.com/vastraacostumes/" target='_blank'><img className='w-[50px]' src="/instagram.svg" alt="instagram" /></a>
-                                <a href="" target='_blank'><img className='w-[50px]' src="/linkedIn.svg" alt="" /></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="pt-5">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d308.6555004909462!2d77.58361858441181!3d12.933331278119814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae15b504aca34b%3A0x19c69ea8046898c!2sVastraa%20Costumes%20Jayanagar!5e1!3m2!1sen!2sin!4v1727372412427!5m2!1sen!2sin"
+                width="150"
+                height="150"
+                style={{ border: "0" }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Vastraa"
+              ></iframe>
             </div>
-        </section>
-    )
+          </div>
+          <div className="lg:flex lg:space-x-20 space-y-8 lg:space-y-0">
+            <div className="lg:flex flex-col items-center space-y-2 hidden">
+              <a
+                href="https://www.facebook.com/vastraacostumes"
+                target="_blank"
+              >
+                <img className="w-[50px]" src="/facebook.svg" alt="facebook" />
+              </a>
+              <a
+                href="https://www.instagram.com/vastraacostumes/"
+                target="_blank"
+              >
+                <img
+                  className="w-[50px]"
+                  src="/instagram.svg"
+                  alt="instagram"
+                />
+              </a>
+              <a href="" target="_blank">
+                <img className="w-[50px]" src="/linkedIn.svg" alt="linkedIn" />
+              </a>
+            </div>
+            <div className="md:flex space-x-20 hidden col-span-2">
+              <div className="lg:flex lg:flex-col lg:space-y-7">
+                <a href="#costumes">
+                  <p className="hover:text-gradient">Costumes</p>
+                </a>
+                <a href="#events">
+                  <p className="hover:text-gradient">Event Management</p>
+                </a>
+                <a href="#whyVastraa">
+                  <p className="hover:text-gradient">Why Vastraa?</p>
+                </a>
+                <a href="#team">
+                  <p className="hover:text-gradient">Team</p>
+                </a>
+                <Link to="/blogs" onClick={() => window.scrollTo(0, 0)}>
+                  <p className="hover:text-gradient">Blogs</p>
+                </Link>
+              </div>
+            </div>
+            <div className="flex flex-col space-y-5 col-span-3">
+              <div className="flex space-x-2 items-center">
+                {<MapPin className="text-[#00AC47]" />}
+                <h2 className="text-[#FFFFFF] md:font-semibold font-bold lg:text-2xl">
+                  Jayanagar
+                </h2>
+              </div>
+              <p className="text-[14px]">
+                #69, 9th Main Rd,
+                <br /> Jayanagar 3rd Block East,
+                <br /> Bengaluru, Karnataka <br />
+                560011
+              </p>
+              <div>
+                <p className="font-bold flex flex-col text-[14px]">
+                  +91-9986178988
+                </p>
+                <a href="" className="cursor-pointer">
+                  vastraaevents@gmail.com
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col space-y-5 col-span-3">
+              <div className="flex space-x-2 items-center">
+                {<MapPin className="text-[#00AC47]" />}
+                <h2 className="text-[#FFFFFF] md:font-semibold font-bold lg:text-2xl">
+                  Whitefiled
+                </h2>
+              </div>
+              <p className="text-[14px]">
+                No A/2, Inner Cir Rd,
+                <br /> Whitefield, Dodsworth Layout,
+                <br /> Bengaluru, Karnataka
+                <br /> 560066
+              </p>
+              <div>
+                <p className="flex flex-col font-bold text-[14px]">
+                  +91-9986178988
+                </p>
+                <a href="" className="cursor-pointer">
+                  vastraaevents@gmail.com
+                </a>
+              </div>
+
+              <div className="flex space-x-3 md:hidden">
+                <a
+                  href="https://www.facebook.com/vastraacostumes"
+                  target="_blank"
+                >
+                  <img
+                    className="w-[50px]"
+                    src="/facebook.svg"
+                    alt="facebook"
+                  />
+                </a>
+                <a
+                  href="https://www.instagram.com/vastraacostumes/"
+                  target="_blank"
+                >
+                  <img
+                    className="w-[50px]"
+                    src="/instagram.svg"
+                    alt="instagram"
+                  />
+                </a>
+                <a href="" target="_blank">
+                  <img className="w-[50px]" src="/linkedIn.svg" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -1,47 +1,59 @@
-import React, { useState } from 'react';
-import './Whatsapp.css';
-import { Gift, MessageCircleX } from 'lucide-react';
+import React, { useState } from "react";
+import "./Whatsapp.css";
+import { Gift, MessageCircleX } from "lucide-react";
 
 const Whatsapp: React.FC = () => {
-    const [isChatbotVisible, setIsChatbotVisible] = useState<boolean>(false);
+  const [isChatbotVisible, setIsChatbotVisible] = useState<boolean>(false);
 
-    const messages = {
-        type: 'incoming',
-        text: 'Hi there 👋\nWelcome to Vastraa!'
-    }
+  const messages = {
+    type: "incoming",
+    text: "Hi there 👋\nWelcome to Vastraa!",
+  };
 
-    const handleToggleChatbot = () => {
-        setIsChatbotVisible(!isChatbotVisible);
-    };
+  const handleToggleChatbot = () => {
+    setIsChatbotVisible(!isChatbotVisible);
+  };
 
-    return (
-        <div className={`app ${isChatbotVisible ? 'show-chatbot' : ''}`}>
-            <button className="chatbot-toggler" onClick={handleToggleChatbot}>
-                {isChatbotVisible ? (
-                    <span><MessageCircleX className='w-10 h-10 text-[#25D366]' /></span>
-                ) : (
-                    <img src="/whatsapp.svg" alt="whatsapp " />
-                )}
-            </button>
-            <div className="chatbot md:w-[400px] md:m-0 md:h-[350px] w-[280px] h-[300px] m-[65px] md:rounded-[12px] rounded-lg">
-                <header className='bg-[#25D366] text-[#FBFFFE]'>
-                    <h2 className='md:font-medium font-bold md:text-xl text-lg'>Connect with us for <br /> Costumes and Events</h2>
-                </header>
-                <ul className="chatbox">
-                    <li className={`chat ${messages.type}`}>
-                        {messages.type === 'incoming' && <span className="material-symbols-outlined"><Gift className='text-[#25D366]' /></span>}
-                        <p className='md:text-xl lg:font-normal'>{messages.text}</p>
-                    </li>
-                </ul>
-                <div className="chat-input">
-                    <a rel="noreferrer" className='bg-[#25D366] font-normal text-white px-6 py-2 w-full lg:h-[48px] flex justify-center items-center md:text-xl text-lg' href="https://wa.me/919986178988" target='_blank'>
-                        <button className="h-10">Click Here For Enquiry</button>
-                    </a>
-                </div>
-            </div>
+  return (
+    <div className={`app ${isChatbotVisible ? "show-chatbot" : ""}`}>
+      <button className="chatbot-toggler" onClick={handleToggleChatbot}>
+        {isChatbotVisible ? (
+          <span>
+            <MessageCircleX className="w-10 h-10 text-[#25D366]" />
+          </span>
+        ) : (
+          <img src="/whatsapp.svg" alt="whatsapp " />
+        )}
+      </button>
+      <div className="chatbot md:w-[400px] md:m-0 md:h-[350px] w-[280px] h-[300px] m-[65px] md:rounded-[12px] rounded-lg">
+        <header className="bg-[#25D366] text-[#FBFFFE]">
+          <h2 className="md:font-medium font-bold text-base">
+            Connect with us for <br /> Costumes and Events
+          </h2>
+        </header>
+        <ul className="chatbox">
+          <li className={`chat ${messages.type}`}>
+            {messages.type === "incoming" && (
+              <span className="material-symbols-outlined">
+                <Gift className="text-[#25D366]" />
+              </span>
+            )}
+            <p className="text-base lg:font-normal">{messages.text}</p>
+          </li>
+        </ul>
+        <div className="chat-input">
+          <a
+            rel="noreferrer"
+            className="bg-[#25D366] font-normal text-white px-6 py-2 w-full lg:h-[48px] flex justify-center items-center md:text-xl text-lg"
+            href="https://wa.me/919019558988"
+            target="_blank"
+          >
+            <button className="h-10 text-base">Click Here For Enquiry</button>
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-
-export default Whatsapp
+export default Whatsapp;
